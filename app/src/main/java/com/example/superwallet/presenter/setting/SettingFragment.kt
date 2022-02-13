d941395ec5ec8470982a063ef858a97c2cd06add
k4keye <vps32@naver.com>
2022-02-13 15:18:04 +0900

    setting page init

--- app/src/main/java/com/example/superwallet/presenter/setting/SettingFragment.kt
+++ app/src/main/java/com/example/superwallet/presenter/setting/SettingFragment.kt
@@ -6,15 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.superwallet.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingFragment : Fragment() {
 
     companion object {
         fun newInstance() = SettingFragment()
     }
 
-    private lateinit var viewModel: SettingViewModel
+    private val viewModel: SettingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +28,6 @@ class SettingFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
--- app/src/main/java/com/example/superwallet/presenter/setting/SettingViewModel.kt
+++ app/src/main/java/com/example/superwallet/presenter/setting/SettingViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.superwallet.presenter.setting
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class SettingViewModel : ViewModel() {
+@HiltViewModel
+class SettingViewModel @Inject constructor(): ViewModel() {
     // TODO: Implement the ViewModel
 }
